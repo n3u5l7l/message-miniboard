d65f88e5c9e075d0196c0d54375544a50eaf9c44
MingMing <greeteaz567@gmail.com>
2022-09-05 00:34:25 -0500

    Added post handler

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,9 @@ router.get('/new', function(req, res, next) {
   res.render('form');
 })
 
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+  res.redirect('/');
+});
+
 module.exports = router;
